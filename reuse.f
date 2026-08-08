cac1b34110a3999078a3713112b6bf7de4a11d12
Foez Ahmed <foez.official@gmail.com>
2026-08-08 11:51:45 +0600

    auto-update

--- reuse.f
+++ reuse.f
@@ -1,3 +1,4 @@
+${ADN_CLK_RST}/assertion/dummy_checker.sv
 ${ADN_CLK_RST}/interface/dummy_interface.sv
 ${ADN_CLK_RST}/source/adn_clk_rst_clk_div.sv
 ${ADN_CLK_RST}/source/adn_clk_rst_dual_edge_register.sv
