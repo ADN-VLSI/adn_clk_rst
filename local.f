64bf093d2243abd7047e06bb6af42858215a5e29
Md. Sakib Hasan Shawon <mdsakibhasanshawon20@gmail.com>
2026-09-08 10:44:01 +0600

    ADD testbench: adn_clk_rst_mux_tb

--- local.f
+++ local.f
@@ -1,3 +1,5 @@
 -i ${ADN_CLK_RST}/testbench
+${ADN_CLK_RST}/testbench/adn_clk_rst_clk_mux_tb.sv
+${ADN_CLK_RST}/testbench/adn_clk_rst_delay_generator_tb.sv
 ${ADN_CLK_RST}/testbench/adn_clk_rst_clk_div_tb.sv
 ${ADN_CLK_RST}/testbench/dummy_tb.sv
